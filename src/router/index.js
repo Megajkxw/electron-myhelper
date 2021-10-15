83862d6c865bffe8e6b3d60467a69c921d078390
Ouge <Ouge@LAPTOP-2SGAAE77>
2021-10-15 20:18:40 +0800

    路由拆分文件写

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,41 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
+import manager from "./manager";
 // import FastOpen from "../views/FileView/FastOpen";
-const routes = [
-  {path: '/', redirect: '/fastopen'},
-  {
-    path: '/fastopen',
-    name: 'FastOpen',
-    component: () => import('../views/FileView/FastOpen')
-  }
-  ,
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+let routes = [
+  // {path: '/', redirect: '/'},
+  // manager[1],
+  // {
+  //   path: '/',
+  //   name: 'Index',
+  //   component: () => import('../views/Manager/Index'),
+  //   children:[
+  //     {
+  //       path: 'fastopen',
+  //       component: () => import('../views/Manager/FileView/FastOpen')
+  //     },
+  //     {
+  //       path: 'about',
+  //       component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  //     }
+  //   ]
+  // },
+
+  // {
+  //       path: '/fastopen',
+  //       name: 'FastOpen',
+  //       component: () => import('../views/Manager/FileView/FastOpen')
+  //     }
+  //   // ,
+  //   // {
+  //   //   path: '/about',
+  //   //   name: 'About',
+  //   //   // route level code-splitting
+  //   //   // this generates a separate chunk (about.[hash].js) for this route
+  //   //   // which is lazy-loaded when the route is visited.
+  //   //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  //   // }
 ]
+routes=routes.concat(manager)
 
 const router = createRouter({
   history: createWebHashHistory(),
---
+++ src/views/Manager/Index.vue
@@ -0,0 +1,14 @@
+<template>
+ssssssss
+</template>
+
+<script>
+    // import ManagerBase from "../../components/ManagerBase";
+    export default {
+        name: "Index",
+    }
+</script>
+
+<style scoped>
+
+</style>
