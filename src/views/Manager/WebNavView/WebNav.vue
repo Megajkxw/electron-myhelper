6c7ac509f0b3561aea488c613b27739200c78fe5
Ouge <Ouge@LAPTOP-2SGAAE77>
2022-03-06 10:16:13 +0800

    引入选项卡

--- src/views/Manager/WebNavView/WebNav.vue
+++ src/views/Manager/WebNavView/WebNav.vue
@@ -1,6 +1,28 @@
 <template>
     <div>
-        <i class="fa fa-home fa-lg"></i>
+<!--        <i class="fa fa-home fa-lg"></i>-->
+        <el-tabs type="border-card">
+            <el-tab-pane label="用户管理">
+                <div class="panel">
+                    
+                </div>
+            </el-tab-pane>
+            <el-tab-pane label="配置管理">
+                <div class="panel">
+
+                </div>
+            </el-tab-pane>
+            <el-tab-pane label="角色管理">
+                <div class="panel">
+
+                </div>
+            </el-tab-pane>
+            <el-tab-pane label="定时任务补偿">
+                <div class="panel">
+
+                </div>
+            </el-tab-pane>
+        </el-tabs>
     </div>
 </template>
 
@@ -11,5 +33,7 @@
 </script>
 
 <style scoped>
-
+    .panel{
+        height: 78.6vh;
+    }
 </style>
